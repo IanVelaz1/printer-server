ebd4e95686aa3d085fb29ea09747c6678d61ac29
ianvelazquez <lestrada@ax.opticalux.com.mx>
2020-02-04 17:48:18 -0600

    added commited

--- routes/notesRoutes.js
+++ routes/notesRoutes.js
@@ -1,6 +1,32 @@
 const Note = require('../models/notesModel.js');
-module.exports = (app) =>{
-   app.get('/api/note',(req,res)=>{
-        
+const {
+   verificaToken
+} = require('../middlewares/authMiddleware');
+
+module.exports = (app) => {
+
+   app.get('/api/note/:id', verificaToken, (req, res) => {
+      let id = req.params.id;
+
+      if (id) {
+         Note.findNote(id, (error, success) => {
+            if (error) {
+               return res.status(400).json({
+                  ok: false,
+                  err: error,
+                  status: 400
+               });
+            }
+            res.status(200).json({
+               ok: true,
+               success
+            });
+         });
+      }
    });
+
+   app.post('/api/note',verificaToken,(req,res)=>{
+       
+   });
+
 }
