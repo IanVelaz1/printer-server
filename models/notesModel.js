02de90f914f11d9854db5d47b0fb5c747b029d83
ianvelazquez <lestrada@ax.opticalux.com.mx>
2020-02-05 16:00:23 -0600

    added more notes routes

--- models/notesModel.js
+++ models/notesModel.js
@@ -4,15 +4,15 @@ const notesSchema = new mongoose.Schema({
 
     fileName: {
         type: String,
-        required:[true,'El nombre del archivo es necesario']
+        required: [true, 'El nombre del archivo es necesario']
     },
     quantity: {
         type: Number,
-        required: [true,'La cantidad es necesaria']
+        required: [true, 'La cantidad es necesaria']
     },
     pieceMeasures: {
         type: Array,
-        required:[true, 'Las medidas son necesarias'],
+        required: [true, 'Las medidas son necesarias'],
         default: []
     },
     priceMeterSquares: {
@@ -20,43 +20,53 @@ const notesSchema = new mongoose.Schema({
         default: 0
     },
     totalMeterSquares: {
-        type:Number,
+        type: Number,
         default: 0
     },
     unitPrice: {
-        type:Number,
+        type: Number,
         default: 0
     },
     unitTotalPrice: {
-        type:Number,
+        type: Number,
         default: 0
     },
-    client:{
-        type:String,
-        required:[true,'El cliente es necesario']
+    client: {
+        type: String,
+        required: [true, 'El cliente es necesario']
     },
-    date:{
-        type:Date,
-        default: new Date()
+    noteDate: {
+        type: String,
+        required: [true, 'La fecha es necesaria']
     }
 
 });
 
-const Note = module.exports = mongoose.model('notes',notesSchema);
+const Note = module.exports = mongoose.model('notes', notesSchema);
 
 
-module.exports.createNote = (note,callback) => {
-    Note.create(note,callback);
+module.exports.createNote = (note, callback) => {
+    Note.create(note, callback);
 }
 
-module.exports.findNote = (id,callback) => {
+module.exports.findNoteById = (id, callback) => {
     Note.findById(id, callback);
 }
 
-module.exports.editNote = (id,note,callback) => {
-    Note.findByIdAndUpdate(id,note,callback);
+module.exports.findNotes = (id, date, callback) => {
+    Note.find({
+        $or: [{
+            _id: id
+        }, {
+            noteDate: date
+        }]
+    }, callback);
 }
 
-module.exports.deleteNote = (id,callback) => {
-    Note.findByIdAndRemove(id,callback);
+module.exports.editNote = (id, note, callback) => {
+    Note.findByIdAndUpdate(id, note, callback);
+}
+
+module.exports.deleteNote = (id, callback) => {
+    Note.findByIdAndRemove(id, callback);
 }
--- routes/notesRoutes.js
+++ routes/notesRoutes.js
@@ -5,25 +5,26 @@ const {
 
 module.exports = (app) => {
 
-   app.get('/api/note/:id', verificaToken, (req, res) => {
-      let id = req.params.id;
+   app.get('/api/note', verificaToken, (req, res) => {
+      let id = req.query.id || '';
+      let date = req.query.date || '';
 
-      if (id) {
-         Note.findNote(id, (error, success) => {
-            if (error) {
-               return res.status(400).json({
-                  ok: false,
-                  err: error,
-                  status: 400
-               });
-            }
-            res.status(200).json({
-               ok: true,
-               success
+      Note.findNotes(id, date, (error, success) => {
+         if (error) {
+            return res.status(400).json({
+               ok: false,
+               err: error,
+               status: 400
             });
+         }
+         res.status(200).json({
+            ok: true,
+            success
          });
-      }
+      });
+
    });
+
 
    app.post('/api/note', verificaToken, (req, res) => {
       const body = req.body;
@@ -34,17 +35,56 @@ module.exports = (app) => {
                ok: false,
                err: error,
                message: error.errors.message,
-               status:400
+               status: 400
             });
          } else {
             res.status(200).json({
                ok: true,
-               status:200,
+               status: 200,
                success
             });
          }
 
       });
+   });
+
+   app.get('api/note/:id', verificaToken, (req, res) => {
+      let id = req.params.id;
+      Note.findNoteById(id, (error, success) => {
+         if (error) {
+            res.status(400).json({
+               ok: false,
+               err: error,
+               status: 400
+            });
+         } else {
+            res.status(200).json({
+               ok: true,
+               success
+            });
+         }
+      });
+   });
+
+   app.put('api/note/:id',(req,res)=>{
+      let id = req.params.id;
+      let note = req.body;
+
+      Note.editNote(id,note,(error,success)=> {
+         if (error) {
+            res.status(400).json({
+               ok: false,
+               err: error,
+               status: 400
+            });
+         } else {
+            res.status(200).json({
+               ok: true,
+               success
+            });
+         }
+      });
+
    });
 
 }
