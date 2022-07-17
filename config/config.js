fda435244596a4266a7f6f778214f0963fe87089
Ian Eduardo Velazquez <ivelazquezenriquez@griddynamics.com>
2022-07-17 14:10:36 -0500

    commit last changes

--- config/config.js
+++ config/config.js
@@ -1,5 +1,7 @@
 module.exports = {
-    db:'mongodb+srv://jansolote:Morrowind5@cluster0.ej9x0.mongodb.net/?retryWrites=true&w=majority',
+    db:'mongodb://localhost:27017/printerdb',
     secretTk : 'secretoContra',
     expTime : 60*30*24
 }
+
+//mongodb+srv://jansolote:Morrowind5@cluster0.ej9x0.mongodb.net/?retryWrites=true&w=majority
