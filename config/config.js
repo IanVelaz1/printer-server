7ab1ef6403b1f32243ad8b7ff0d641d439a07ac6
Ian Eduardo Velazquez <ivelazquezenriquez@griddynamics.com>
2022-07-20 18:43:34 -0500

    changed db address

--- config/config.js
+++ config/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    db:'mongodb://localhost:27017/printerDb',
+    db:'mongodb+srv://jansolote:Morrowind5@cluster0.ej9x0.mongodb.net/?retryWrites=true&w=majorityb',
     secretTk : 'secretoContra',
     expTime : 60*30*24
 }
